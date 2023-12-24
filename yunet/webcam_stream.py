288f62abfa1b63457fef5ba670e39a38c38ee7df
bergb15 <brianvdberg2107@gmail.com>
2023-12-24 17:07:43 +0100

    Yunet webcam stream

---
+++ yunet/webcam_stream.py
@@ -0,0 +1,57 @@
+import os
+import time
+import cv2
+
+# Define the width and height
+WIDTH = 640
+HEIGHT = 480
+
+# Initialize the YuNet model
+detector = cv2.FaceDetectorYN.create(
+    model=      os.path.join('models', 'face_detection_yunet_2023mar.onnx'),
+    config=     "",
+    input_size= (WIDTH, HEIGHT)
+)
+
+# Initialize the webcam stream
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+# Set the stream properties
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+
+# Initialize FPS Counter
+prevFrame = time.time()
+curFrame = time.time()
+
+while True:
+    # Get a frame
+    ret, frame = cap.read()
+    if ret:
+        # Manage FPS counter
+        curFrame = time.time()
+        fps = 1 / (curFrame - prevFrame)
+        prevFrame = curFrame
+        cv2.putText(frame, "FPS: {:.2f}".format(fps), (8, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1, cv2.LINE_AA)
+
+        # Perform detection on the frame with YuNet
+        _, faces = detector.detect(frame)
+
+        # Draw rectangles
+        if faces is not None: 
+            for face in faces:
+                # bouding box
+                box = list(map(int, face[:4]))
+                color = (0, 255, 0)
+                cv2.rectangle(frame, box, color, 1)
+
+        # Display the Result
+        cv2.imshow("Display", frame)
+
+    # Check for exit key 'q'
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+# Clean the application
+cv2.destroyAllWindows()
